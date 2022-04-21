de134501c76e33cfa0d1e25db27d2039bfb8cb64
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2022-04-21 13:53:15 +0200

    init api

---
+++ APIAircraft/src/main/java/com/example/APIAircraft/ApiAircraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIAircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAircraftApplication.class, args);
+	}
+
+}
---
+++ APIAircraft/src/test/java/com/example/APIAircraft/ApiAircraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIAircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAircraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
